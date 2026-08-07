947f966feea019b0c9c8f3c5dbaa7dcc5daa5c50
Zuhair Abbas <syedzuhairabbas02@gmail.com>
2026-08-08 01:00:21 +0500

    Server Ready And Database Connected

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app =  express()
+const cors = require('cors')
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("Database Connected"))
+.catch((err) => console.log("Database Connection failed error: " ,  err))
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT , () => {
+    console.log("Express Server Running")
+})
+
+app.get('/' , (req , res) => {
+    res.json({ message: "Express Server Running" })
+})
