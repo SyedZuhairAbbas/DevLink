066a9ef93bd3636adca5446f36d12f36afa6049a
Zuhair Abbas <syedzuhairabbas02@gmail.com>
2026-08-08 17:18:59 +0500

    Add Project CRUD routes with validation

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const projectRoutes = require('./routes/projectRoutes')
 const app =  express()
 const cors = require('cors')
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use(express.json())
 app.use(cors())
+app.use('/projects' , projectRoutes)
 
 app.listen(process.env.PORT , () => {
     console.log("Express Server Running")
