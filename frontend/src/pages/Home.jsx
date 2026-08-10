75b0d8c3a39dee3e58228d76d89d7b9d2fb140be
Zuhair Abbas <syedzuhairabbas02@gmail.com>
2026-08-11 00:00:49 +0500

    Added Home page with project fetching from Express API

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,32 @@
+import  {useState , useEffect } from  'react'
 
 export default function Home(){
-    return  <p>Home</p>
+    const [projects , setProjects ] = useState(null)
+    const [isProject , setIsProjects ] = useState(false)
+
+    useEffect( () => {
+        fetch('http://localhost:3000/projects/')
+        .then(res => res.json())
+        .then(data => { 
+            setProjects(data)
+            setIsProjects(true)
+        })
+    } , [])
+
+
+    return (
+        <div  className='p-8'>
+            {isProject ? (
+                <div>
+                    {projects.map(  project => (
+                        <div key={project._id}>
+                            <h1>{project.title}</h1>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p> Loading Projects ...</p>
+            )}
+        </div>
+    )
 }
